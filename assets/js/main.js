67b84f968a5a9614b376de20a1e5efe1ddc0c42c
Mason <mason.mideumlee@gmail.com>
2014-11-03 12:13:50 -0800

    first deploy to heroku

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+	$(window).load(function() {
+		$("#myModal").modal('show');
+	});
+
+
+
+	$('a.choice').click(function () {
+		$('a.choice').removeClass('selected');
+		$(this).addClass('selected');
+		current = $(this);
+		
+		$('#wrapper').scrollTo($(this).attr('href'), 800);		
+		
+		return false;
+	});
+
+	$(window).resize(function () {
+		resizePanel();
+	});
+});
+
+function resizePanel() {
+	width = $(window).width();
+	height = $(window).height();
+
+	mask_width = width * $('.item').length;
+	$('#debug').html(width  + ' ' + height + ' ' + mask_width);
+	$('#wrapper, .item').css({width: width, height: height});
+	$('#mask').css({width: mask_width, height: height});
+	$('#wrapper').scrollTo($('a.selected').attr('href'), 0);
+}
