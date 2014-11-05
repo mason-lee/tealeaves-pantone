0b34f6159c66cc27e98976a7e2ff447110a08d63
Mason <mason.mideumlee@gmail.com>
2014-11-04 16:23:51 -0800

    changed pipeline for icomoon

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -15,28 +15,32 @@
 // (if you're using LESS with the built-in default config, you'll want
 //  to change `assets/styles/importer.less` instead.)
 var cssFilesToInject = [
-  'styles/**/*.css'
+	'styles/**/*.css',
+	'styles/**/*.eot',
+	'styles/**/*.svg',
+	'styles/**/*.ttf',
+	'styles/**/*.woff'
 ];
 
 
 // Client-side javascript files to inject in order
 // (uses Grunt-style wildcard/glob/splat expressions)
 var jsFilesToInject = [
-  
-  // Load sails.io before everything else
-  'js/dependencies/sails.io.js',
-  'js/dependencies/jquery.min.js',
-  'js/dependencies/jquery.min.map.js',
-  'js/dependencies/jquery.scrollTo.js',
-  'js/dependencies/jquery.scrollmagic.js',
+	
+	// Load sails.io before everything else
+	'js/dependencies/sails.io.js',
+	'js/dependencies/jquery.min.js',
+	'js/dependencies/jquery.min.map.js',
+	'js/dependencies/jquery.scrollTo.js',
+	'js/dependencies/jquery.scrollmagic.js',
 
-  // Dependencies like jQuery, or Angular are brought in here
-  'js/dependencies/**/*.js',
-  'js/bootstrap/**/*.js',
-  'js/lib/**/*.js',
-  // All of the rest of your client-side js files
-  // will be injected here in no particular order.
-  'js/**/*.js'
+	// Dependencies like jQuery, or Angular are brought in here
+	'js/dependencies/**/*.js',
+	'js/bootstrap/**/*.js',
+	'js/lib/**/*.js',
+	// All of the rest of your client-side js files
+	// will be injected here in no particular order.
+	'js/**/*.js'
 ];
 
 
@@ -50,7 +54,7 @@ var jsFilesToInject = [
 // templates get spit out to the same file.  Be sure and check out `tasks/README.md`
 // for information on customizing and installing new tasks.
 var templateFilesToInject = [
-  'templates/**/*.html'
+	'templates/**/*.html'
 ];
 
 
@@ -59,11 +63,11 @@ var templateFilesToInject = [
 // (i.e. where the other Grunt tasks spit them out, or in some cases, where
 // they reside in the first place)
 module.exports.cssFilesToInject = cssFilesToInject.map(function(path) {
-  return '.tmp/public/' + path;
+	return '.tmp/public/' + path;
 });
 module.exports.jsFilesToInject = jsFilesToInject.map(function(path) {
-  return '.tmp/public/' + path;
+	return '.tmp/public/' + path;
 });
 module.exports.templateFilesToInject = templateFilesToInject.map(function(path) {
-  return 'assets/' + path;
+	return 'assets/' + path;
 });
